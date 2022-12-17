ff7311c5d042095734fc33ac669d8cbba95e723e
arijeetbasu <2105845@reva.edu.in>
2022-12-17 20:02:30 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -355,12 +355,12 @@ mailChimp();
       });
 
     
-       var cursor = document.getElementById('cursor');
-       document.addEventListener('mousemove', function(e){
-       var x = e.clientX;
-       var y = e.clientY;
-       cursor.style.left = x + 'px';
-       cursor.style.top = y + 'px';
-       })
+      //  var cursor = document.getElementById('cursor');
+      //  document.addEventListener('mousemove', function(e){
+      //  var x = e.clientX;
+      //  var y = e.clientY;
+      //  cursor.style.left = x + 'px';
+      //  cursor.style.top = y + 'px';
+      //  })
 
     })(jQuery);	
